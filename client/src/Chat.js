b518284e2d36e04e22db14220647f4b6468b0f07
Kevin Summers <kevi4196@stud.kea.dk>
2022-05-18 15:07:03 +0200

    React Client Beginning

---
+++ client/src/Chat.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Chat(socket, username, chatroom) {
+  
+    return (
+    <div></div>
+  )
+}
+
+export default Chat
--- server/app.js
+++ server/app.js
@@ -1,15 +1,41 @@
 import "dotenv/config";
 
-
 import express from "express";
-
 const app = express();
 app.use(express.json());
 
-import mongoose from "mongoose";
+import cors from "cors";
+app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017/chatland", { 
-    useNewUrlParser: true })
+import http from "http";
+const server = http.createServer(app);
+
+import { Server } from "socket.io";
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST", "PUT", "DELETE"]
+    }
+});
+
+io.on("connect", (socket) => {
+    console.log("User Connected: ", socket.id);
+
+    socket.on("join_chatroom", (data) => {
+        socket.join(data);
+        console.log("User with id: ", socket.id, " joined the chatroom", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected: ", socket.id);
+    });
+});
+
+
+import mongoose from "mongoose";
+mongoose.connect("mongodb://localhost:27017/chatland", {
+    useNewUrlParser: true
+})
     .then((_) => console.log("Connected to DB"))
     .catch((err) => console.error("error", err));
 
@@ -17,8 +43,7 @@ mongoose.connect("mongodb://localhost:27017/chatland", {
 import userRouter from "./routers/userRouter.js";
 app.use("/auth", userRouter);
 
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log("Starting server on port:", PORT);
+server.listen(8080, () => {
+    console.log("Server Running")
 });
